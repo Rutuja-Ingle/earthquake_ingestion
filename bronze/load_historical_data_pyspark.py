d40e6a6b6bbe96b7efe81ee5831a028d969b21ab
Rutuja <inglerutuja024@gmail.com>
2024-10-20 17:35:08 +0530

    Devvelopment completed for historical load pyspark - demo

--- bronze/load_historical_data_pyspark.py
+++ bronze/load_historical_data_pyspark.py
@@ -1,1 +1,12 @@
 from pyspark.sql import SparkSession
+import argparse
+import requests
+
+
+if __name__ == '__main__':
+
+       # Define spark session
+        spark = SparkSession.builder.master("local[*]").appName("Historical Load").getOrCreate()
+
+
+       #          
